a15f81294bd701a0d0c70e45a12de075a90ba2f4
Denis John <denis.john.ndarake@gmail.com>
2023-01-09 17:24:38 +0100

    load config from env file

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,6 +7,37 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgres://root:secret@localhost:5432/todo?sslmode=disable"
+)
+
+var testQueries *Queries
+
+// TestMain: main entrypoint of the test
+func TestMain(m *testing.M) {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("Can not connect to DB")
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
+
+/***
+package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	_ "github.com/lib/pq"
 	"github.com/ndenisj/go_todo/utils"
 )
 
@@ -29,3 +60,5 @@ func TestMain(m *testing.M) {
 
 	os.Exit(m.Run())
 }
+
+*/
